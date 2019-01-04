bb652c8b0e69f6cab0884f0ebf88f78692ee67c7
Nickzhong789 <nickzhong789@sina.com>
2019-01-04 11:10:06 +0800

    add installation and start files

---
+++ AnnoTool.bat
@@ -0,0 +1,2 @@
+cd ImgAnnoTool
+python main.py
---
+++ installation/install.bat
@@ -0,0 +1,4 @@
+pip install --upgrade setuptools -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+
+
